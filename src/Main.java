47f4f1f98a07d1858da29a0671c12bb3427a1c3b
vudep <work.vupham@gmail.com>
2021-06-16 19:54:04 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Main{
+    double DA = 0.12d;
+    double HRA = 150.0d;
+    double TA = 120.0d;
+    double other = 450.0d;
+    double tax_PF = 0.14d;
+    double tax_IT = 0.15d;
+    double net_salary = 0.0d; // to store the salary after calculating
+
+    double calculate_net_salary(double basic_salary){
+        this.net_salary = Math.floor(basic_salary + this.HRA + this.TA + this.other - (this.tax_IT + this.tax_PF - this.DA)*basic_salary);
+        return this.net_salary;
+    }
+
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        Main staff_salary = new Main();
+
+        double basic_salary = 0.0d;
+        double net_salary = 0.0d;
+
+        System.out.print("enter your basic salary: ");
+        basic_salary = Double.parseDouble(scn.nextLine());
+        net_salary = staff_salary.calculate_net_salary(basic_salary);
+
+        System.out.println("here is you salary ");
+        System.out.println("Basic salary: " + basic_salary);
+        System.out.println("Tax:\n" +
+                "     PF(" + Math.floor(staff_salary.tax_PF*100) +"% of basic salary): -" + Math.floor(staff_salary.tax_PF*basic_salary) + "$\n" +
+                "     IT(" + Math.floor(staff_salary.tax_IT*100) +"% of basic salary): -" + Math.floor(staff_salary.tax_IT*basic_salary) + "$");
+
+
+
+    }
+}
